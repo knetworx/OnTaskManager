9f0876adffda5cef836ea96f9007ebbce3b9a9bb
Nick Heilmann <nick.heilmann@gmail.com>
2026-05-27 22:46:12 -0700

    Iteration

--- tests/test_storage.cpp
+++ tests/test_storage.cpp
@@ -10,15 +10,17 @@ TEST(Storage, OpensAndInsertsSample) {
     const auto path = std::filesystem::temp_directory_path() / "ontask_test.db";
     std::filesystem::remove(path);
 
-    ontask::storage::Database db(path);
-    ontask::storage::ActivityRepository repo(db);
+    {
+        ontask::storage::Database db(path);
+        ontask::storage::ActivityRepository repo(db);
 
-    ontask::tracker::ActivitySample s;
-    s.processName = L"test.exe";
-    s.windowTitle = L"unit test";
-    s.timestamp = std::chrono::system_clock::now();
+        ontask::tracker::ActivitySample s;
+        s.processName = L"test.exe";
+        s.windowTitle = L"unit test";
+        s.timestamp = std::chrono::system_clock::now();
 
-    EXPECT_NO_THROW(repo.insert(s, std::chrono::seconds(0)));
+        EXPECT_NO_THROW(repo.insert(s, std::chrono::seconds(0)));
+    }
 
     std::filesystem::remove(path);
 }
